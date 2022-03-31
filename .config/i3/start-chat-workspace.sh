6543fcf2e23b459b8d6a998cc712827d2c678c60
Bobby Brockway <bbrockway@mintel.com>
2022-03-31 14:38:22 +0100

    backup before reinstall

---
+++ .config/i3/start-chat-workspace.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+i3-msg 'workspace 9; append_layout /home/bbrockway/.config/i3/chat-workspace.json'
+nohup google-chrome-stable --app=https://mail.google.com/chat/u/0/#chat/welcome &
+nohup google-chrome-stable --app=https://www.flowdock.com &
+nohup google-chrome-stable --app=https://workflowy.com/#/80677c003b45 &
+#nohup google-chrome-stable --app=https://tweetdeck.twitter.com/ &
